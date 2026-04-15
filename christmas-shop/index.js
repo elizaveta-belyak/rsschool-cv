6e38addb0387227e8191da2a1f6cd6fbba65b787
elizaveta-belyak <elizavettabelyak@gmail.com>
2026-04-15 13:48:17 +0400

    feat: add mobile burger menu

--- christmas-shop/index.js
+++ christmas-shop/index.js
@@ -21,3 +21,40 @@ function updateTimer() {
 updateTimer();
 
 setInterval(updateTimer, 1000);
+
+
+
+/* --- БУРГЕР МЕНЮ --- */
+const navOpenBtn = document.getElementById('nav-open');   // Иконка бургера
+const navCloseBtn = document.getElementById('nav-close'); // Крестик в меню
+const navMobileMenu = document.getElementById('nav-mobile'); // Само меню
+const bodyElement = document.body;
+
+if (navOpenBtn) {
+    navOpenBtn.addEventListener('click', () => {
+        navMobileMenu.classList.add('active');
+        bodyElement.classList.add('noscroll');
+    });
+}
+
+if (navCloseBtn) {
+    navCloseBtn.addEventListener('click', () => {
+        navMobileMenu.classList.remove('active');
+        bodyElement.classList.remove('noscroll'); 
+    });
+}
+
+const mobileMenuLinks = document.querySelectorAll('.nav-mobile__link');
+mobileMenuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navMobileMenu.classList.remove('active');
+        bodyElement.classList.remove('noscroll');
+    });
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 768) {
+        navMobileMenu.classList.remove('active');
+        bodyElement.classList.remove('noscroll');
+    }
+});
